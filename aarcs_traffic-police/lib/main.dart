9350aaa8830470f2693b9bc26333b58ce13f9d35
unknown <11prem>
2025-09-30 10:04:35 +0530

    fixed minor issues affecting the emergency_responce_screen.dart

--- aarcs_traffic-police/lib/main.dart
+++ aarcs_traffic-police/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+git import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/dashboard.dart';
 
